ac41d484e424c194dcca1ddce0e4e418ffcd7b77
Shawn <shawnaugustine01@gmail.com>
2023-02-01 09:16:34 +0000

    fixed class names

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,21 +22,21 @@ const Search = ({ filterGames }) => {
             <label className="search__label" htmlFor="search__input">Game:</label>
             <input 
                 type="search" 
-                placeholder="Type game name..." id="search__input" 
+                placeholder="Type game name..." className="search__input" 
                 value={searchNameTerm}
                 onChange={event => setSearchNameTerm(event.target.value)} />
 
             <label className="search__label" htmlFor="search__input">Publisher:</label>
             <input 
                 type="search" 
-                placeholder="Type publisher here..." id="search__input" 
+                placeholder="Type publisher here..." className="search__input" 
                 value={searchPublisherTerm}
                 onChange={event => setSearchPublisherTerm(event.target.value)} />
 
             <label className="search__label" htmlFor="search__input">Genre:</label>
             <input 
                 type="search" 
-                placeholder="Type genre here..." id="search__input" 
+                placeholder="Type genre here..." className="search__input" 
                 value={searchGenreTerm}
                 onChange={event => setSearchGenreTerm(event.target.value)} />
 
